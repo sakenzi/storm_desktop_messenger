5418f36962a90ecf91245eae33572d16bfdf549e
sakenzi <ermekulysaken8@gmail.com>
2025-05-15 13:48:50 +0600

    Добавлено icon.py

---
+++ views/main_window/icons/icon.py
@@ -0,0 +1,11 @@
+from utils.context_util import resource_path
+
+
+ICONS = {
+    'favourite': resource_path("icons/icons/bookmark-ribbon.png"),
+    'group': resource_path("icons/icons/team-28.png"),
+    'night_theme': resource_path("icons/icons/evening-icon.svg"),
+    'customization': resource_path("icons/icons/597182.png"),
+    'add': resource_path("icons/icons/plus-math.png"),
+    'search': resource_path("icons/icons/icons8-поиск.svg")
+}
